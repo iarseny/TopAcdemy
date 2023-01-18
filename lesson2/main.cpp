178b820db038d1328c88bd2bf9df3d20b180856c
iarseny <122807698+iarseny@users.noreply.github.com>
2023-01-18 16:07:11 +0000

    Update

--- lesson2/main.cpp
+++ lesson2/main.cpp
@@ -27,7 +27,36 @@ class Array {
         }
     }
 
+    void insert(int index,int cherepaxa) {
+        if (index + 1 <= this-> size) {
+            this->arr[index] = cherepaxa;
+        } else {
+            std::cout << "много";
+        }
+    }
+
+    void delete_index(int index) {
+        if (index + 1 < this->size) {
+            int *tmp = this->arr;
+            this->arr = new int[size - 1];
+            delete [] this->arr;
+            size--;
+            for (int i = 0; i < index + 1; i++) {
+                this->arr[i] = tmp[i];
+            }
+
+            for (int j = index + 1; j < size + 1; j++) {
+                this->arr[j - 1] = tmp[j];
+            }
+
+            delete [] tmp;
+        } else {
+            std::cout << "черепаха, много";
+        }
+    }
+
     ~Array() {
+        delete [] this->arr;
         std::cout << std::endl << "Объект удалён" << std::endl;
     }
 
@@ -42,6 +71,17 @@ int main() {
     array.add_back(3);
     array.add_back(4);
     array.add_back(5);
+
+    array.print();
+    array.insert(1,8);
+
+    std::cout << std::endl << std::endl;
+
+    array.print();
+
+    std::cout << std::endl << std::endl;
+
+    array.delete_index(0);
 
     array.print();
 }
