7e27e111404b301f60c3c0c0c75f4a058c8a8a5c
iarseny <122807698+iarseny@users.noreply.github.com>
2023-01-31 12:02:28 +0000

    doing homework

---
+++ Homework 6/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class Worker {
+    std::string name;
+    std::string surname;
+    std::string money;
+    std::string dol;
+    std::string otchestvo;
+    int size = 1;
+    Worker *array;
+    public:
+    Worker(const Worker &worker) {
+        
+    }
+};
