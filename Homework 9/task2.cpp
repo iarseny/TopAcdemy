5e69b776f15d11f8360b80342a584d953506f4e3
iarseny <122807698+iarseny@users.noreply.github.com>
2023-02-06 16:38:54 +0000

    commit

---
+++ Homework 9/task2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <string>
+
+
+int main() {
+
+    std::ifstream stream_read;
+
+    std::string main_string;
+
+    stream_read.open("file1.txt");
+
+    if (stream_read.is_open()) {
+        while (stream_read.good()) {
+            std::string tmp = "";
+            std::getline(stream_read,tmp);
+            main_string += tmp + "\n";
+        }
+    }
+
+
+    stream_read.close();
+
+    int n = std::count(main_string.begin(), main_string.end(), '\n');
+
+    int size = main_string.length() - n;
+
+    n--;
+
+    std::cout << size;
+
+    std::cout << n;
+
+    return 0;
+}
